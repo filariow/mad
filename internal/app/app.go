fbe0e8dfc8e5e9ae470fec0b566ce2e3bcd74c47
Francesco <fr.ilario@gmail.com>
2022-01-09 02:54:54 +0100

    Add table shortcut

--- internal/app/app.go
+++ internal/app/app.go
@@ -25,6 +25,7 @@ const appID = "com.filariow.mad"
 
 type app struct {
 	a                    *gtk.Application
+	builder              *gtk.Builder
 	mainWindow           *gtk.ApplicationWindow
 	editorContainer      *gtk.Viewport
 	editor               *gtk.TextView
@@ -37,6 +38,7 @@ type app struct {
 	horizontal_box       *gtk.Box
 	cssProvider          *gtk.CssProvider
 	textBuffer           *gtk.TextBuffer
+	tableDialog          *gtk.Dialog
 
 	history          history.History
 	trapTextChanges  bool
@@ -265,11 +267,71 @@ func (a *app) activate() {
 }
 
 func (a *app) addTable() {
-	t := `| header_1 | header_2 | header_3 |
-| ---              | ---               | ---              |
-| 1,1               | 1,2               | 1,3              |
+	// builder
+	b, err := gtk.BuilderNewFromString(views.Mad)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-`
+	do, err := b.GetObject("dialog_table_size")
+	if err != nil {
+		log.Fatalf("can not get object with id 'dialog_table_size': %s", err)
+	}
+
+	d, ok := do.(*gtk.Dialog)
+	if !ok {
+		log.Fatal("object with id 'dialog_table_size' is not a gtk.Dialog")
+	}
+
+	// dialog table cols spin button
+	csbo, err := b.GetObject("dialog_table_cols_spin")
+	if err != nil {
+		log.Fatalf("can not get object with id 'dialog_table_cols_spin': %s", err)
+	}
+
+	csb, ok := csbo.(*gtk.SpinButton)
+	if !ok {
+		log.Fatal("object with id 'dialog_table_cols_spin' is not a gtk.SpinButton")
+	}
+
+	// dialog table rows spin button
+	rsbo, err := b.GetObject("dialog_table_rows_spin")
+	if err != nil {
+		log.Fatalf("can not get object with id 'dialog_table_rows_spin': %s", err)
+	}
+
+	rsb, ok := rsbo.(*gtk.SpinButton)
+	if !ok {
+		log.Fatal("object with id 'dialog_table_rows_spin' is not a gtk.SpinButton")
+	}
+
+	defer d.Destroy()
+	if r := d.Run(); r != gtk.RESPONSE_OK {
+		return
+	}
+
+	cc := csb.GetValueAsInt()
+	rr := rsb.GetValueAsInt()
+
+	t := "|"
+	for c := 1; c <= cc; c++ {
+		t += " header |"
+	}
+	t += "\n|"
+
+	for c := 1; c <= cc; c++ {
+		t += " ---          |"
+	}
+	t += "\n|"
+
+	for r := 1; r <= rr; r++ {
+		for c := 1; c <= cc; c++ {
+			t += " cell        |"
+		}
+		t += "\n|"
+	}
+	t += "\n"
+
 	a.textBuffer.InsertAtCursor(t)
 }
 
@@ -520,6 +582,17 @@ func (a *app) loadUI() {
 	}
 
 	a.mainWindow = w
+
+	do, err := b.GetObject("dialog_table_size")
+	if err != nil {
+		log.Fatalf("can not get object with id 'dialog_table_size': %s", err)
+	}
+
+	d, ok := do.(*gtk.Dialog)
+	if !ok {
+		log.Fatal("object with id 'dialog_table_size' is not a gtk.Dialog")
+	}
+	a.tableDialog = d
 }
 
 func (a *app) applyDeltaToFont(delta int) {
